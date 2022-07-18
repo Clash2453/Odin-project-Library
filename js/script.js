10fe11288028d9173f2c5b2c5f903fe47e6c50a1
Clash2453 <spasmilenkov123@gmail.com>
2022-07-18 22:51:10 +0300

    Change the behavior of main title

--- js/script.js
+++ js/script.js
@@ -3,22 +3,17 @@ const input = document.getElementById('input-card');
 const initialization = document.getElementById('initialize');
 const addButton = document.getElementById('add-button');
 const createButton = document.getElementById('createButton');
+const mainTitle = document.getElementById('main-title');
 
 function promptInput(){
     input.classList.remove('hidden');
     container.classList.remove('hidden');
-    // while(initialization.firstChild){
-    //     initialization.removeChild(initialization.firstChild);
-    // }
     initialization.style.flexDirection = 'row';
     initialization.style.justifyContent = 'space-between'
-
 }
 
 function createLibrary(){
     input.classList.add('hidden');
     container.style.backgroundImage = 'url("../images/shelf.jpg")';
-    const mainTitle = document.createElement('h1');
     mainTitle.textContent = 'Your Library'
-    initialization.appendChild(mainTitle)
 }
