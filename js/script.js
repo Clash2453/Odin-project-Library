ecd150c90cb6a83b641c8f974f13280ce03022a9
Clash2453 <spasmilenkov123@gmail.com>
2022-07-20 22:52:37 +0300

    Add info-card close button

--- js/script.js
+++ js/script.js
@@ -5,9 +5,19 @@ const input = document.getElementById('input-card');
 const initialization = document.getElementById('initialize');
 const addButton = document.getElementById('add-button');
 const createButton = document.getElementById('createButton');
+const closeButton = document.getElementById('close-button');
 const mainTitle = document.getElementById('main-title');
+
+const notes = document.getElementById('info-notes');
+const read = document.getElementById('info-read');
+const pages = document.getElementById('info-pages');
+const author = document.getElementById('info-author');
+const title = document.getElementById('info-title');
+const infoCard = document.getElementById('info-card');
+
 let myBooks = [];
 
+closeButton.addEventListener('click', closeInfo);
 addButton.addEventListener('click', createLibrary)
 
 //The book class
@@ -81,22 +91,21 @@ function  addCover(){
 
 function showInfo(){
     const thisBook = myBooks[this.value]
-    const infoCard = document.getElementById('info-card');
 
-    const title = document.getElementById('info-title');
     title.textContent = thisBook.title;
-
-    const author = document.getElementById('info-author');
     author.textContent = thisBook.author;
-
-    const pages = document.getElementById('info-pages');
     pages.textContent = thisBook.pages;
-
-    const read = document.getElementById('info-read');
     read.textContent = thisBook.read;
-
-    const notes = document.getElementById('info-notes');
     notes.textContent = thisBook.notes;  
     
     infoCard.classList.remove('hidden');
+}
+
+function closeInfo(){
+    title.textContent = '';
+    author.textContent = '';
+    pages.textContent = '';
+    read.textContent = '';
+    notes.textContent = '';  
+    infoCard.classList.add('hidden')
 }
